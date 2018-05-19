08c3fb1ec988f1b98eaaad4154538168fac89aa3
Igor Petrovic <petrovic00@gmail.com>
2018-05-19 19:54:14 +0200

    fix apt:key id value

--- profile/manifests/elasticsearch.pp
+++ profile/manifests/elasticsearch.pp
@@ -1,6 +1,6 @@
 class profile::elasticsearch{
     apt::key { 'elasticsearch':
-      id     => '4609 5ACC 8548 582C 1A26 99A9 D27D 666C D88E 42B4'
+      id     => '4609 5ACC 8548 582C 1A26 99A9 D27D 666C D88E 42B4',
       server => 'artifacts.elastic.co',
       source => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
       notify => Exec['apt_update'],
