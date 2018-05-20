9ea161df0844d600c0fc72051b8268ec831957a3
Igor Petrovic <petrovic00@gmail.com>
2018-05-20 21:21:47 +0200

    fix elasticsearch install

--- profile/manifests/elasticsearch.pp
+++ profile/manifests/elasticsearch.pp
@@ -1,28 +1,11 @@
 class profile::elasticsearch{
-    #include ::apt
-    #
-    #apt::key { 'elasticsearch':
-    #  id     => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
-    #  server => 'artifacts.elastic.co',
-    #  source => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
-    #}->
-    #  exec { "add-elasticsearch-repo":
-    #  command => '/bin/echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list',
-    #  onlyif  => "/usr/bin/test ! -f /etc/apt/sources.list.d/elastic-6.x.list",
-    #  notify => Exec['apt_update'],
-    #}->
-    #class { 'java':
-    #  distribution => 'jre',
-    #}->
-    #package{'apt-transport-https': ensure => 'installed'}->
-    #package{'elasticsearch': ensure => 'installed'}
-    
     # REQUIREMENTS
     # puppet module elastic-elasticsearch
     # puppet module puppetlabs-java
     include ::java
 
     class { 'elasticsearch': }
-    elasticsearch::instance { 'es-01': }
-    
+    elasticsearch::instance { 'es-01':
+      status => 'running',
+    }
 }
