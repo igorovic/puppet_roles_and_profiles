5b5038acdd460d2f4171b447f3d60dcd43affb38
Igor Petrovic <petrovic00@gmail.com>
2018-05-23 15:21:48 +0200

    nodejs install profile

--- profile/manifests/nodejs.pp
+++ profile/manifests/nodejs.pp
@@ -1,6 +1,6 @@
 class profile::nodejs {
     # REQUIREMENTS
-    # puppet module puppet-nodejs
-    #   install #> puppet module install puppet-nodejs -i "$(puppet config print environmentpath)/$(puppet config print environment)/modules"
+    # puppet module willdurand-nodejs
+    #   install #> puppet module install willdurand-nodejs -i "$(puppet config print environmentpath)/$(puppet config print environment)/modules"
     class { 'nodejs': }
 }
