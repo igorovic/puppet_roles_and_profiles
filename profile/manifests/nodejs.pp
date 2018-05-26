3e86669c64b21961fa2fbdfedb4a21a0993e54a0
Igor Petrovic <petrovic00@gmail.com>
2018-05-26 11:56:54 +0200

    always install latest version of nodejs

--- profile/manifests/nodejs.pp
+++ profile/manifests/nodejs.pp
@@ -2,5 +2,7 @@ class profile::nodejs {
     # REQUIREMENTS
     # puppet module willdurand-nodejs
     #   install #> puppet module install willdurand-nodejs -i "$(puppet config print environmentpath)/$(puppet config print environment)/modules"
-    class { 'nodejs': }
+    class { 'nodejs':
+      version =>"latest"
+    }
 }
